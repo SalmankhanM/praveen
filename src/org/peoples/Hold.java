40e7117b13e470f4e3de88a98070357688137083
SalmankhanM <salmankhan9586@gmail.com>
2021-01-21 12:53:16 +0530

    done

---
+++ src/org/peoples/Hold.java
@@ -0,0 +1,22 @@
+package org.peoples;
+
+public class Hold {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=1;
+		int b=2;
+		int c=3;
+		int d=4;
+		int e=5;
+		int f=6;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+
+	}
+
+}
